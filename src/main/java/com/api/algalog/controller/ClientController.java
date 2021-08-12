6a0be173776979e1731e197e2d1157bbec956c00
Matheus <matheus002_@hotmail.com>
2021-08-12 16:32:39 -0300

    chore: add route to update a client

--- src/main/java/com/api/algalog/controller/ClientController.java
+++ src/main/java/com/api/algalog/controller/ClientController.java
@@ -3,10 +3,15 @@ package com.api.algalog.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.algalog.domain.model.Client;
@@ -32,5 +37,23 @@ public class ClientController {
     			.map(ResponseEntity::ok)
     			.orElse(ResponseEntity.notFound().build());
 
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Client addClient(@RequestBody Client client) {
+    	return clientRepository.save(client);
+    }
+    
+    @PutMapping("/{clientId}")
+    public ResponseEntity<Client> update(@PathVariable Long clientId, @RequestBody Client client){
+    	if (!clientRepository.existsById(clientId)) {
+    		return ResponseEntity.notFound().build();
+    	}
+    	
+    	client.setId(clientId);
+    	client = clientRepository.save(client);
+    	
+    	return ResponseEntity.ok(client);
     }
 }
