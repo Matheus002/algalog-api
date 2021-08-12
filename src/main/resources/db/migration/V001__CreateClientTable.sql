24b0323935705da956d5a628682af98c0a5f2acb
Matheus <matheus002_@hotmail.com>
2021-08-12 13:53:46 -0300

    chore: add flyway and initial migrations

---
+++ src/main/resources/db/migration/V001__CreateClientTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE client (
+    id bigint not null AUTO_INCREMENT,
+    nome varchar(60) not null,
+    email varchar(255) not null,
+    phone_number varchar(20) not null,
+    
+    primary key (id)
+)
