1f5299534faff95b1087c10f2324256d86d0d6d9
Matheus <matheus002_@hotmail.com>
2021-08-12 17:27:28 -0300

    chore: add Bean validator

--- src/main/java/com/api/algalog/controller/ClientController.java
+++ src/main/java/com/api/algalog/controller/ClientController.java
@@ -3,6 +3,8 @@ package com.api.algalog.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -42,7 +44,7 @@ public class ClientController {
     
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Client addClient(@RequestBody Client client) {
+    public Client addClient(@Valid @RequestBody Client client) {
     	return clientRepository.save(client);
     }
     
