e51037aa11e35b32f35185f5ae7060c05ae48cb2
Matheus <matheus002_@hotmail.com>
2021-08-12 16:08:11 -0300

    chore: add route that return one client

--- src/main/java/com/api/algalog/controller/ClientController.java
+++ src/main/java/com/api/algalog/controller/ClientController.java
@@ -3,11 +3,10 @@ package com.api.algalog.controller;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.algalog.domain.model.Client;
@@ -17,12 +16,21 @@ import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/clients")
 public class ClientController {
 	
 	private ClientRepository clientRepository;
 
-    @GetMapping("/clients")
+    @GetMapping
     public List<Client> listar() {
        return clientRepository.findAll();
+    }
+    
+    @GetMapping("/{clientId}")
+    public ResponseEntity<Client> search(@PathVariable Long clientId) {
+    	return clientRepository.findById(clientId)
+    			.map(ResponseEntity::ok)
+    			.orElse(ResponseEntity.notFound().build());
+
     }
 }
